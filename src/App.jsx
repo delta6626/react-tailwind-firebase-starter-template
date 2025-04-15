5eede3e9a7f4414d5068e5413941da4f20f1729a
Delta <58563252+delta6626@users.noreply.github.com>
2025-04-16 00:38:33 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-amber-50">
+      <h1 className="animate-bounce font-bold text-4xl">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
